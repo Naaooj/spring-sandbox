0243c4c059c02c37377b2a1ba28e971136f4d323
jbernez <johann.bernez@gmail.com>
2016-09-26 21:50:35 +0200

    Added spring org/hibernate em/liquibase

--- src/main/java/fr/naoj/spring/sandbox/config/PersistenceConfiguration.java
+++ src/main/java/fr/naoj/spring/sandbox/config/PersistenceConfiguration.java
@@ -1,15 +1,23 @@
 package fr.naoj.spring.sandbox.config;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import liquibase.integration.spring.SpringLiquibase;
 
 /**
  * @author Johann Bernez
@@ -18,24 +26,55 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan("fr.naoj.spring.sandbox.persistence")
 public class PersistenceConfiguration {
+	
+	@Autowired
+	private Environment env;
 
 	@Bean(name = "dataSource")
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-        	.setType(EmbeddedDatabaseType.HSQL)
-        	.setName("embeddedDb")
-            .addScript("classpath:schema.sql")
-            .addScript("classpath:data.sql")
-            .build();
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.pass"));
+		return dataSource;
+	}
+	
+	@Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "fr.naoj.spring.sandbox.persistence" });
+        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        final Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        em.setJpaProperties(hibernateProperties);
+        return em;
+    }
+	
+	@Bean
+    public JpaTransactionManager transactionManager() {
+        final JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
     }
 
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
-    
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	@Bean
+	public SpringLiquibase liquibase() {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:data/liquibase-changeLog.xml");
+		liquibase.setDataSource(dataSource());
+		return liquibase;
+	}
 }
